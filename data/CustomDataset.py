2f3878c35613604b4bf2bbf7a406b33d1e6ef679
Lamberto31 <lamberto.preite@studenti.unisalento.it>
2024-07-03 18:25:08 +0200

    Activated np_img_read with cv2

--- data/CustomDataset.py
+++ data/CustomDataset.py
@@ -60,8 +60,8 @@ class CustomImageDataset(Dataset):
       img_name = self.imgs_labels.iloc[idx, 0]
       img_path = os.path.join(self.imgs_dir, img_name)
       # Read image file
-      img_file = read_image(img_path)
-      #img_file = np_img_read(img_path)
+      #img_file = read_image(img_path)
+      img_file = np_img_read(img_path)
       # Get image label
       label = self.imgs_labels.iloc[idx, 1]
       # Apply transformations
