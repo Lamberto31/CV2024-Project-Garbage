306f7399d582fe145e6c4c1a3288635e6eff65a1
Lamberto31 <lamberto.preite@studenti.unisalento.it>
2024-07-22 20:11:44 +0200

    Now the create_dataset script can accept a third argument to skip mv confirmation

--- create_dataset.sh
+++ create_dataset.sh
@@ -17,14 +17,28 @@ if [ ! -d "$2" ]; then
     exit 1
 fi
 
-# Prompt the user to confirm the move
-echo "Are you sure you want to move all files from subdirectories of '$1' to '$2'? (y/n)"
-read -r response
-if [ "$response" != "y" ]; then
-    echo "Operation cancelled."
-    exit 1
+# Check if a third argument is provided and use it to automatically confirm the move
+if [ $# -eq 3 ] && [ "$3" == "-y" ]; then
+    auto_response="y"
+else
+    auto_response="n"
 fi
 
+# Count the number of files in the subdirectories
+file_count=$(find "$1" -type f ! -name "*.json" | wc -l)
+
+# Prompt the user to confirm the move
+echo "Are you sure you want to move all files ($file_count) from subdirectories of '$1' to '$2'? (y/n)"
+if [ "$auto_response" == "y" ]; then
+    echo "Move automatically confirmed."
+    response="y"
+else
+    read -r response
+    if [ "$response" != "y" ]; then
+        echo "Operation cancelled."
+        exit 1
+    fi
+fi
 # Move files from subdirectories to the output directory, excluding JSON files
 find "$1" -type f ! -name "*.json" -exec mv {} "$2" \;
 
