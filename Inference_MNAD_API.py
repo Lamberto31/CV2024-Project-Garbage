500df3a6da1230ab437d8a7c6eca2cfee2c64427
Lamberto31 <lamberto.preite@studenti.unisalento.it>
2024-07-25 18:55:12 +0200

    Added Inference_MNAD_API.py to call the inference using HTTP POST

---
+++ Inference_MNAD_API.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+from Inference_MNAD import *
+
+app = Flask(__name__)
+
+@app.route('/api/predict', methods=['POST'])
+def predict():
+    # Get the image file from the request
+    image_file = request.files['image']
+
+    # Save the image file to disk
+    image_file_path = './dataset/single/images/test.jpg'
+    image_file.save(image_file_path)
+
+    # Forward the image to the deep learning model for prediction
+    result = classify(image_file_path)
+
+    # Return the prediction result as a response
+    if result == 1:
+        return 'Dirty'
+    else:
+        return 'Clean'
+
+if __name__ == '__main__':
+    app.run()
